9197226bdb900d6427df408d6980f24859363a6d
devmunro <defmunro@gmail.com>
2023-09-28 22:54:26 +0100

    added spline room

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spline scene="https://prod.spline.design/6PD56Nd4uOFNfWbn/scene.splinecode" />
     </div>
   );
 }
